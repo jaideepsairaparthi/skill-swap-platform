4de1712a2ee4f6f9bcd292e92fbdce2ad22c9957
jaideepsai11 <jaideepsairaparthi1123@gmail.com>
2025-02-23 03:02:15 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Skill Swap Platform</h1>
+      <p>Welcome to the Skill Swap Platform!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
