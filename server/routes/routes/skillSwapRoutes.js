1022466f20124856183d2bf0ba8749ab16e10186
jaideepsai11 <jaideepsairaparthi1123@gmail.com>
2025-03-13 12:01:00 +0530

    changed skillswap

---
+++ server/routes/routes/skillSwapRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { body } = require('express-validator');
+const { requestSkillSwap } = require('../controllers/skillSwapController');
+const authenticate = require('../middlewares/auth');
+const validate = require('../middlewares/validate');
+
+const router = express.Router();
+
+// Request Skill Swap
+router.post(
+  '/skill-swap/request',
+  authenticate,
+  validate([
+    body('targetUserId').notEmpty().withMessage('Target user ID is required'),
+    body('skillName').notEmpty().withMessage('Skill name is required'), // Use skillName instead of skillId
+  ]),
+  requestSkillSwap
+);
+
+module.exports = router;
--- server/routes/skillRoutes.js
+++ server/routes/skillRoutes.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const { body } = require('express-validator');
-const { requestSkillSwap } = require('../controllers/skillSwapController');
+const { getAllSkills } = require('../controllers/skillController');
 const authenticate = require('../middlewares/auth');
-const validate = require('../middlewares/validate');
 
 const router = express.Router();
 
-// Request Skill Swap
-router.post(
-  '/skill-swap/request',
-  authenticate,
-  validate([
-    body('targetUserId').notEmpty().withMessage('Target user ID is required'),
-    body('skillName').notEmpty().withMessage('Skill name is required'), // Use skillName instead of skillId
-  ]),
-  requestSkillSwap
-);
+// Get All Skills
+router.get('/skills', authenticate, getAllSkills);
 
 module.exports = router;
